2066f5815966b1c555070a0c71e0ef858c327390
Thomas Koshy <ttkoshy@gmail.com>
2023-08-21 17:46:52 -0400

    Select Bar and Name Search working

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-import axios from 'axios';
-
 const name = document.querySelector('.name');
 const symbol = document.querySelector('.symbol');
 const rank = document.querySelector('.rank');
@@ -7,33 +5,84 @@ const priceUSD = document.querySelector('.priceUSD');
 const priceChange1D = document.querySelector('.priceChange1D');
 const input = document.querySelector('.input')
 const submit = document.querySelector('.submit')
+const card = document.querySelector('.card');
+const select = document.querySelector('.cryptocurrencies');
 
-submit.addEventListener('click', (event) => {
+let coins;
+
+submit.addEventListener('click', async (event) => {
     event.preventDefault();
-    let userInput = parseInt(input.value); //convert input into integer
-    fetchChain(userInput)
-    input.value = ''; //clear the input after submission
-})
+    const userInput = input.value.trim();
 
-async function fetchChain(userInput) {
-    try {
-        const response = await axios.get(
-        "https://api.coinstats.app/public/v1/coins?skip=0&limit=2000&currency=USD"
-        )
-        console.log(response);
-        const data = await response.json()
-        console.log(data)
-        displayAPI(data.coins, userInput)
-    } catch(err) {
-        alert("Blockchain not reached");
+        if (userInput !== "") {
+            try {
+                const response = await fetch (
+                "https://api.coinstats.app/public/v1/coins?skip=0&limit=2000&currency=USD"
+                );
+                const data = await response.json()
+                
+                coins = data.coins
+
+                populateCryptoSelect(data.coins);
+
+                if (!isNaN(userInput)) {
+                    displayAPI(coins, userInput)
+                } else {
+                    fetchName(coins, userInput)
+                }
+                input.value = ''; //clear the input
+        } catch(err) {
+            console.error("API not reached", err);
+            card.textContent = "Something went wrong"
+        }
     }
+});
+
+function populateCryptoSelect(coins) {
+    select.innerHTML = '<option value="" selected>Choose a cryptocurrency</option>';
+
+    coins.forEach((coin, index) => {
+        const option = document.createElement('option');
+        option.value = index.toString();
+        option.textContent = coin.name;
+        select.appendChild(option);
+    })
 }
 
+select.addEventListener('change', () => {
+    const selectedIndex = select.value;
+    if (selectedIndex !== '') {
+        displayAPI(coins, selectedIndex)
+    }
+})
 
-function displayAPI(data, userInput) {
-   name.textContent = 'Name: ' + data[userInput - 1].name
-   symbol.textContent = 'Symbol: ' + data[userInput - 1].symbol
-   rank.textContent = 'Rank: ' + data[userInput - 1].rank;
-   priceUSD.textContent = 'Price (USD): ' + data[userInput - 1].price
-   priceChange1D.textContent = 'Price Change 1 Day: ' + data[userInput - 1].priceChange1d 
+function displayAPI(data, selectedIndex) {
+   const selectedCoin = data[selectedIndex]; 
+   name.textContent = 'Name: ' + selectedCoin.name
+   symbol.textContent = 'Symbol: ' + selectedCoin.symbol
+   rank.textContent = 'Rank: ' + selectedCoin.rank;
+   priceUSD.textContent = 'Price (USD): ' + selectedCoin.price
+   priceChange1D.textContent = 'Price Change 1 Day: ' + selectedCoin.priceChange1d 
+}
+
+async function fetchName(data, userInput) {
+    try {
+        const selectedCoin = data.find(coin => coin.name === userInput);
+        if (selectedCoin) {
+            const response = await fetch (
+                `https://api.coinstats.app/public/v1/coins/${selectedCoin.id}?currency=USD`
+            )
+            const coinData = await response.json();
+            console.log(coinData)
+            name.textContent = 'Name: ' + coinData.coin.name
+            symbol.textContent = 'Symbol: ' + coinData.coin.symbol
+            rank.textContent = 'Rank: ' + coinData.coin.rank;
+            priceUSD.textContent = 'Price (USD): ' + coinData.coin.price
+            priceChange1D.textContent = 'Price Change 1 Day: ' + coinData.coin.priceChange1d 
+        } else {
+            console.error("Name not found")
+        }
+    } catch(error) {
+        console.error("Error fetching coin details", error)
+    }
 }
